9e524dedb24aab9d6a9a7ef260ee6b8118480ab2
Gustavo Martínez <tavo962@gmail.com>
2016-11-06 21:00:18 -0600

    Add SASS

--- structure/head.php
+++ structure/head.php
@@ -24,9 +24,7 @@
 	<meta property="og:url" content="<?php get_location();?>" />
 <?php endif; unset( $og );?>
 
-<!-- <link href="<?php bloginfo( 'stylesheet_url' );?>" rel="stylesheet" /> -->
+<link href="<?php bloginfo( 'stylesheet_url' );?>" rel="stylesheet" />
 <link href="https://cdnjs.cloudflare.com/ajax/libs/fancybox/2.1.5/jquery.fancybox.min.css" rel="stylesheet" />
-<link href="<?php bloginfo( 'template_url' );?>/less/app.less" type="text/css" rel="stylesheet/less" />
-<script src="<?php bloginfo( 'template_url' );?>/js/lib/less.min.js"></script>
 
 <link rel="shortcut icon" href="<?php image_dir();?>/favicon.png" />
--- structure/scripts.php
+++ structure/scripts.php
@@ -1,11 +1,11 @@
 <!-- <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBMfSy7sintdAewYeUU7515DH7i1J2XpG4"></script>
 <script src="js/map.min.js"></script> -->
 <script src="https://www.google.com/recaptcha/api.js"></script>
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/fancybox/2.1.5/jquery.fancybox.min.js"></script>
 <script src="<?php bloginfo( 'template_url' );?>/js/lib/bootstrap.min.js"></script>
-<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.5.3/angular.min.js"></script>
-<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.5.3/angular-sanitize.min.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.5.8/angular.min.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.5.8/angular-sanitize.min.js"></script>
 <script src="<?php bloginfo( 'template_url' );?>/js/app.js"></script>
 <script src="<?php bloginfo( 'template_url' );?>/js/factories.js"></script>
 <script src="<?php bloginfo( 'template_url' );?>/js/controllers.js"></script>
@@ -13,9 +13,9 @@
 <!-- Facebook -->
 <div id="fb-root"></div>
 <script>(function(d, s, id) {
-  var js, fjs = d.getElementsByTagName(s)[0];
-  if (d.getElementById(id)) return;
-  js = d.createElement(s); js.id = id;
-  js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.5";
-  fjs.parentNode.insertBefore(js, fjs);
+var js, fjs = d.getElementsByTagName(s)[0];
+if (d.getElementById(id)) return;
+js = d.createElement(s); js.id = id;
+js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.5";
+fjs.parentNode.insertBefore(js, fjs);
 }(document, 'script', 'facebook-jssdk'));</script>
