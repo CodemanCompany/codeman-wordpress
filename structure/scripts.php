29a570b36ac2f6d5729b5b57b9fbf53345fd68fb
Gustavo Martínez <tavo962@gmail.com>
2017-09-06 01:36:38 -0500

    Replace Bower by NPM

--- structure/scripts.php
+++ structure/scripts.php
@@ -2,11 +2,11 @@
 <!-- <script src="https://maps.googleapis.com/maps/api/js?key=CODEMAN_API_KEY"></script>
 <script src="js/map.min.js"></script> -->
 <script src="https://www.google.com/recaptcha/api.js"></script>
-<script src="<?php bloginfo( 'template_url' );?>/bower_components/jquery/dist/jquery.min.js"></script>
-<script src="<?php bloginfo( 'template_url' );?>/bower_components/fancybox/dist/jquery.fancybox.min.js"></script>
-<script src="<?php bloginfo( 'template_url' );?>/bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
-<script src="<?php bloginfo( 'template_url' );?>/bower_components/angular/angular.min.js"></script>
-<script src="<?php bloginfo( 'template_url' );?>/bower_components/angular-sanitize/angular-sanitize.min.js"></script>
+<script src="<?php bloginfo( 'template_url' );?>/node_modules/jquery/dist/jquery.min.js"></script>
+<script src="<?php bloginfo( 'template_url' );?>/node_modules/fancybox/dist/jquery.fancybox.min.js"></script>
+<script src="<?php bloginfo( 'template_url' );?>/node_modules/bootstrap/dist/js/bootstrap.min.js"></script>
+<script src="<?php bloginfo( 'template_url' );?>/node_modules/angular/angular.min.js"></script>
+<script src="<?php bloginfo( 'template_url' );?>/node_modules/angular-sanitize/angular-sanitize.min.js"></script>
 <script src="<?php bloginfo( 'template_url' );?>/js/app.js"></script>
 <script src="<?php bloginfo( 'template_url' );?>/js/factories.js"></script>
 <script src="<?php bloginfo( 'template_url' );?>/js/controllers.js"></script>
